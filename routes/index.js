2ab71f68b5923b9979792ea8edfabdb2f9fdf712
LKHohn <lkhohn@gmail.com>
2016-02-18 16:26:01 -0500

    signup functionality

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,74 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('signup', function(req, res, next){
-  res.render('signup');
+router.get('/signup', function(req, res, next){
+knex('users').select().then(
+  function(data){
+    res.send(data);
+  });
 });
+
+router.post('/signup', function(req, res, next){
+  var newUser = req.body;
+  hashPassword(newUser, registerUser);
+  function registerUser(){
+    knex('users').insert({
+      username: req.body.username,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      password: req.body.password,
+      phoneNumber: req.body.phoneNumber,
+      city: req.body.city,
+      state: req.body.state,
+      preferredPayment: req.body.preferredPayment
+  }).then(function(data){
+    res.send(data);
+  });
+}
+  function hashPassword(user, callback){
+    bcrypt.genSalt(10, function(err, salt){
+      bcrypt.hash(user.password, salt, function(err, hash){
+        user.password = hash;
+        callback(user);
+        });
+      });
+    }
+});
+
+
 
 router.get('/signin', function(req, res, next){
-  res.render('signin');
+  nex('users').select().then(
+    function(data){
+      res.send(data);
+    });
+  });
+
+router.post('/signin', function(req, res, next){
+  knex('users').first().where({
+    username: req.body.username
+  }).then(function(user){
+    bcrypt.compare(req.body.password, user.password, function(err, match){
+      if(match){
+        // req.session.user eqla to everything you got back form db (its an object) from the previous user query
+        req.session.user = user;
+        // delete key value pair for password
+        delete req.session.user.password;
+        res.send(data);
+      } else {
+        res.send('incorrect');
+      }
+    });
+  });
 });
 
-router.get('/addScout', function(req, res, next){
-  res.render('addScout');
+
+
+router.get('/new', function(req, res, next){
+  res.render('new');
 });
 
+router.post('/new', function(req, res, next){
+  console.log('new post');
+});
 module.exports = router;
